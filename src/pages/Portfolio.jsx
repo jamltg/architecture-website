e0deec8bd2cc1c36783bb09fe2dc715c9530a571
jamltg <jamletigio@gmail.com>
2025-03-19 00:25:23 +0800

    add code to git

---
+++ src/pages/Portfolio.jsx
@@ -0,0 +1,13 @@
+import PortfolioFeatures from "../components/PortfolioFeatures";
+import Footer from "../components/Footer";
+
+function Portfolio() {
+  return (
+    <>
+      <PortfolioFeatures />
+      <Footer />
+    </>
+  );
+}
+
+export default Portfolio;
